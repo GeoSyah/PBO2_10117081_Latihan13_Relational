1af272d7f6d128d3da6dc2ada83e0b44faa26cc6
GeoSyah <43498342+GeoSyah@users.noreply.github.com>
2018-09-23 14:22:05 +0700

    Add files via upload

---
+++ src/PBO2_10117081_Latihan13_Relational.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Geo Syah Alkautsar
+ * 10117081 IF-2
+ * Menampilkan operasi bilangan menggunakan operator relasional
+ */
+public class PBO2_10117081_Latihan13_Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bill = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bill == bil2));
+        System.out.println("a != b = " + (bill != bil2));
+        System.out.println("a > b = " + (bill > bil2));
+        System.out.println("a < b = " + (bill < bil2));
+        System.out.println("b >= a = " + (bill >= bil2));
+        System.out.println("b <= a = " + (bill <= bil2));
+    }
+    
+}
